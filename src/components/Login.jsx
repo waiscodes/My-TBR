8b1da807c064a18af7b160370df7af130cd0710d
waiscodes <b.wais@outlook.com>
2021-01-31 23:47:37 -0700

    added a form for sign up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,13 @@ const Login = () => {
   return (
     <>
       <p>Login</p>
+      <form action='#'>
+        <label htmlFor='email'>Email</label>
+        <input type='text' name='email' id='email' />
+        <label htmlFor='password'>Password</label>
+        <input type='current-password' name='password' id='password' />
+        <input type='submit' value='Submit' />
+      </form>
     </>
   );
 };
