13c83fdd1f3545e559af12ba007cc8d3d5898710
waiscodes <b.wais@outlook.com>
2021-02-01 11:37:28 -0700

    added usestate to get value as well as autocomplete attribute to fields

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import fire from "../fire";
 
 const Login = () => {
-  const auth = fire.auth();
-  console.log(auth);
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
 
-  const changeHandler = () => {};
-  const submitHandler = () => {};
+  console.log(Email, Password);
+
+  const auth = fire.auth();
+
+  const changeHandler = (e) => {
+    switch (e.target.id) {
+      case "email":
+        setEmail(e.target.value);
+        break;
+      case "password":
+        setPassword(e.target.value);
+        break;
+    }
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <>
       <p>Login</p>
       <form onSubmit={submitHandler}>
         <label htmlFor='email'>Email</label>
-        <input type='text' name='email' id='email' onChange={changeHandler} />
+        <input
+          type='email'
+          name='email'
+          id='email'
+          onChange={changeHandler}
+          autoComplete='username'
+        />
         <label htmlFor='password'>Password</label>
         <input
-          type='current-password'
+          type='password'
           name='password'
           id='password'
           onChange={changeHandler}
+          autoComplete='current-password'
         />
         <input type='submit' value='Submit' />
       </form>
