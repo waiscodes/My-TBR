2e5f91ae9e27c2488270a0ebb4ddb0d0ff562964
waiscodes <b.wais@outlook.com>
2021-02-01 19:53:38 -0700

    imported router and added route for signin page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import SignIn from "./views/SignIn.jsx";
 import "./App.css";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <Container
-          className='d-flex align-items-center justify-content-center'
-          style={{ minHeight: "100vh" }}
-        >
-          <div className='w-100' style={{ maxWidth: "400px" }}>
-            <SignIn />
-          </div>
-        </Container>
-      </AuthProvider>
+      <Container
+        className='d-flex align-items-center justify-content-center'
+        style={{ minHeight: "100vh" }}
+      >
+        <div className='w-100' style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path='/' component={SignIn} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
     </>
   );
 }
