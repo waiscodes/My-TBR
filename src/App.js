398e2871ecd41c76992a22ff808cef1c26899da4
waiscodes <b.wais@outlook.com>
2021-02-02 18:41:07 -0700

    moved profile from views to components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Profile from "./views/Profile.jsx";
+import Dashboard from "./views/Dashboard.jsx";
 import PrivateRoute from "./views/PrivateRoute";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
             <AuthProvider>
               <Switch>
                 <Route path='/Login' component={SignIn} />
-                <PrivateRoute exact path='/' component={Profile} />
+                <PrivateRoute exact path='/' component={Dashboard} />
               </Switch>
             </AuthProvider>
           </Router>
---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Profile from "../components/Profile";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Profile />
+      <div className='w-100 text-center mt-2'></div>
+    </>
+  );
+};
+
+export default Dashboard;
