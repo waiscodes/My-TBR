8d538ac62cc9f4925515cd49ac266cad2367dc82
waiscodes <b.wais@outlook.com>
2021-02-02 20:28:22 -0700

    Added an add book field but haven't added logic yet

---
+++ src/components/AddBooks.jsx
@@ -0,0 +1,48 @@
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../context/AuthContext";
+import { useHistory } from "react-router-dom";
+
+const AddBooks = () => {
+  const titleRef = useRef();
+  const authorRef = useRef();
+  const descriptionRef = useRef();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { currentUser } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          {currentUser && currentUser.email}
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id='email'>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type='email'
+                autoComplete='username'
+                ref={titleRef}
+                required
+              />
+            </Form.Group>
+            <Form.Group id='email'>
+              <Form.Label>Author</Form.Label>
+              <Form.Control type='text' ref={authorRef} required />
+            </Form.Group>
+            <Button disabled={loading} className='w-100' type='submit'>
+              Recommend
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default AddBooks;
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Profile from "../components/Profile";
 import BookList from "../components/BookList";
+import AddBooks from "../components/AddBooks";
 
 const Dashboard = () => {
   return (
     <>
       <Profile />
+      <AddBooks />
       <BookList />
       <div className='w-100 text-center mt-2'></div>
     </>
