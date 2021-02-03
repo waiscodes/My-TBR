04a007b85c15bec873cb72ea030cba6e92e8f929
waiscodes <b.wais@outlook.com>
2021-02-02 19:55:52 -0700

    moved code from old project to here to render books

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,9 +1,112 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import fire from "../fire";
 
 const Book = () => {
+  const [title, setTitle] = useState();
+  const [author, setAuthor] = useState();
+  const [desc, setDesc] = useState();
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  // Firebase
+  const db = fire.firestore();
+
+  let booksRef;
+  let unsubscribe;
+
+  booksRef = db.collection("books");
+
+  const addToFirestore = () => {
+    booksRef.add({
+      title: title,
+      author: author,
+      desc: desc,
+    });
+  };
+
+  const getBooks = () => {
+    booksRef.onSnapshot((querySnapshot) => {
+      setBooks(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          book: doc.data(),
+        }))
+      );
+    });
+  };
+
+  // Event Handlers
+
+  const handleFieldChange = (e) => {
+    switch (e.target.id) {
+      case "title":
+        setTitle(e.target.value);
+        break;
+      case "author":
+        setAuthor(e.target.value);
+        break;
+      case "desc":
+        setDesc(e.target.value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addToFirestore();
+  };
+
   return (
     <>
-      <div>Specific Book</div>
+      <div className='container'>
+        {desc}
+        <h2>Birm's Profile</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='title'>Title</label>
+          <input
+            type='text'
+            name='title'
+            id='title'
+            onChange={handleFieldChange}
+          />
+          <br />
+          <label htmlFor='author'>Author</label>
+          <input
+            type='text'
+            name='author'
+            id='author'
+            onChange={handleFieldChange}
+          />
+          <br />
+          <label htmlFor='desc'>Description</label>
+          <textarea
+            name='desc'
+            id='desc'
+            cols='30'
+            rows='10'
+            onChange={handleFieldChange}
+          ></textarea>
+          <br />
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+      <div>
+        {books.map((doc) => {
+          return (
+            <li key={doc.id} id='tbr'>
+              {doc.book.title}
+              <span>{doc.book.title}</span>
+              <span>{doc.book.author}</span>
+              <span>{doc.book.desc}</span>
+            </li>
+          );
+        })}
+      </div>
     </>
   );
 };
