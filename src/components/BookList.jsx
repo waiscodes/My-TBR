c23dde27d6dbd9b373390a9bfaa4ebdba62bff18
waiscodes <b.wais@outlook.com>
2021-02-02 19:51:10 -0700

    created book and book list components and rendered them in dashboard

---
+++ src/components/BookList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Book from "./Book";
+
+const BookList = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <Book />
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default BookList;
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Profile from "../components/Profile";
-import UpdateProfile from "../components/UpdateProfile";
+import BookList from "../components/BookList";
 
 const Dashboard = () => {
   return (
     <>
       <Profile />
-      <UpdateProfile />
+      <BookList />
       <div className='w-100 text-center mt-2'></div>
     </>
   );
