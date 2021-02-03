4dd10ba22b464f8ddb517b6a8c755a23ddf0abb0
waiscodes <b.wais@outlook.com>
2021-02-02 20:04:40 -0700

    removed book and only rendered list in book list

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,14 +1,45 @@
-import React from "react";
-import Book from "./Book";
+import React, { useState, useEffect } from "react";
+import fire from "../fire";
 
 const BookList = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  // Firebase
+  const db = fire.firestore();
+
+  let booksRef;
+
+  booksRef = db.collection("books");
+
+  const getBooks = () => {
+    booksRef.onSnapshot((querySnapshot) => {
+      setBooks(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          book: doc.data(),
+        }))
+      );
+    });
+  };
+
   return (
     <>
-      <ul>
-        <li>
-          <Book />
-        </li>
-      </ul>
+      <div>
+        {books.map((doc) => {
+          return (
+            <li key={doc.id} id='tbr'>
+              {doc.book.title}
+              <span>{doc.book.title}</span>
+              <span>{doc.book.author}</span>
+              <span>{doc.book.desc}</span>
+            </li>
+          );
+        })}
+      </div>
     </>
   );
 };
