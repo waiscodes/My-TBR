390aece168e0841aa1474ac984f68335b272a236
waiscodes <b.wais@outlook.com>
2021-02-04 19:32:07 -0700

    added a toggle to only show add book only when necessary

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -4,12 +4,20 @@ import BookList from "../components/BookList";
 import AddBooks from "../components/AddBooks";
 
 const Dashboard = () => {
+  const [addBook, setAddBook] = useState(false);
+
+  const toggleBook = () => {
+    setAddBook(!addBook);
+  };
+
   return (
     <>
       <YourProfile />
-      <AddBooks />
+      <div className='w-100 text-center mt-2'>
+        <button onClick={toggleBook}>Recommend Book</button>
+      </div>
+      {addBook ? <AddBooks /> : ""}
       <BookList />
-      <div className='w-100 text-center mt-2'></div>
     </>
   );
 };
