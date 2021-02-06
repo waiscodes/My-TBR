e5c181bc4b6f9c8a3e00f789801446ed3f4f5d34
waiscodes <b.wais@outlook.com>
2021-02-06 11:48:21 -0700

    Created profile and added it to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./views/Dashboard.jsx";
+import Profile from "./views/Profile.jsx";
 import PrivateRoute from "./views/PrivateRoute";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
             <AuthProvider>
               <Switch>
                 <Route path='/Login' component={SignIn} />
+                <Route path='/u/:username' component={Profile} />
                 <PrivateRoute exact path='/' component={Dashboard} />
               </Switch>
             </AuthProvider>
